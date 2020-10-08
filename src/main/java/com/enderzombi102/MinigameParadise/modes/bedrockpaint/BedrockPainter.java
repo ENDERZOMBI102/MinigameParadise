08f3becdc77b8b76dab910e4b80d360178384494
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-09 01:00:39 +0200

    started working on new mode

---
+++ src/main/java/com/enderzombi102/MinigameParadise/modes/bedrockpaint/BedrockPainter.java
@@ -0,0 +1,17 @@
+package com.enderzombi102.MinigameParadise.modes.bedrockpaint;
+
+import com.enderzombi102.MinigameParadise.modes.ModeBase;
+
+public class BedrockPainter extends ModeBase {
+
+	public BedrockPainter() {
+
+	}
+
+
+	@Override
+	public void stop() {
+
+
+	}
+}
