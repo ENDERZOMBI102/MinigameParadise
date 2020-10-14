7d9f9cf597ada4a4913b03dec49b37e65dda73b3
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-14 12:59:36 +0200

    now randomize mob drops too

--- src/main/java/com/enderzombi102/MinigameParadise/modes/dropcalipse/Dropcalipse.java
+++ src/main/java/com/enderzombi102/MinigameParadise/modes/dropcalipse/Dropcalipse.java
@@ -1,12 +1,14 @@
 package com.enderzombi102.MinigameParadise.modes.dropcalipse;
 
 import com.enderzombi102.MinigameParadise.modes.ModeBase;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
@@ -27,26 +29,33 @@ public class Dropcalipse extends ModeBase {
 		broadcastPrefixedMessage("the Dropcalipse is started! good luck!");
 	}
 
+	public ItemStack randomize(Item item) {
 
-
+		ItemStack itemStack = item.getItemStack();
+		itemStack.setAmount( new Random().nextInt( this.maxDrops ) );
+		if ( this.randomDrops ) {
+			itemStack.setType( Material.values()[ new Random().nextInt( Material.values().length-1 ) ] );
+		}
+		return itemStack;
+	}
 
 	@Override
 	public void stop() {
 		HandlerList.unregisterAll(this.listener);
 	}
 
-
 	private class DropcalipseListener implements Listener {
+
 		@EventHandler
-		public void OnBlockBreak(BlockDropItemEvent evt) {
-			for ( Item item : evt.getItems() ) {
-				ItemStack itemStack = item.getItemStack();
-				itemStack.setAmount( new Random().nextInt( Dropcalipse.instance.maxDrops ) );
-				if (Dropcalipse.instance.randomDrops) {
-					itemStack.setType( Material.values()[ new Random().nextInt( Material.values().length-1 ) ] );
-				}
-				item.setItemStack( itemStack );
+		public void OnBlockDrop(BlockDropItemEvent evt) {
+			for (Item item : evt.getItems()) {
+				item.setItemStack( randomize(item) );
 			}
+		}
+
+		@EventHandler
+		public void OnEntityDrop(EntityDropItemEvent evt) {
+			evt.getItemDrop().setItemStack( randomize( evt.getItemDrop() ) );
 		}
 	}
 }
