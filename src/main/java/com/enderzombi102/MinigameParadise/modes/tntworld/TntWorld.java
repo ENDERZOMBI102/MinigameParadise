ad345ec2a487c3232aa9b79cb7d27f069295b9a2
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-22 18:33:40 +0200

    now spawn 1 tnt every 4/3 blocks movement

--- src/main/java/com/enderzombi102/MinigameParadise/modes/tntworld/TntWorld.java
+++ src/main/java/com/enderzombi102/MinigameParadise/modes/tntworld/TntWorld.java
@@ -3,15 +3,21 @@ package com.enderzombi102.MinigameParadise.modes.tntworld;
 import com.enderzombi102.MinigameParadise.MinigameParadise;
 import com.enderzombi102.MinigameParadise.modes.ModeBase;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class TntWorld extends ModeBase {
 
 	private final TntWorldListener listener;
+	private static final HashMap<UUID, Integer> playerTntCountdown = new HashMap<>();
 
 
 	public TntWorld() {
@@ -28,10 +34,31 @@ public class TntWorld extends ModeBase {
 	private class TntWorldListener implements Listener {
 		@EventHandler
 		public void onPlayerMove(PlayerMoveEvent evt) {
-			if ( evt.getFrom().getBlock().getLocation() == evt.getTo().getBlock().getLocation() ) {
+			Location oldPos = evt.getFrom(), newPos = evt.getTo();
+			int [] pos = {
+					( ( Double ) oldPos.getX() ).intValue(),
+					( ( Double ) newPos.getX() ).intValue(),
+					( ( Double ) oldPos.getZ() ).intValue(),
+					( ( Double ) newPos.getZ() ).intValue()
+			};
+			if ( pos[0] == pos[1] && pos[2] == pos[3] ) {
 				return;
 			}
-			evt.getPlayer().getLocation().getWorld().spawn( evt.getFrom(), TNTPrimed.class );
+			// get player
+			Player player = evt.getPlayer();
+			// check if the player is present in the hashmap
+			if ( playerTntCountdown.containsKey( player.getUniqueId() ) ) {
+				// it is, do corrisponding action
+				if ( playerTntCountdown.get( player.getUniqueId() ) > 3 ) {
+					player.getLocation().getWorld().spawn( evt.getFrom(), TNTPrimed.class );
+					playerTntCountdown.put( player.getUniqueId(), 0 );
+				} else {
+					playerTntCountdown.put( player.getUniqueId(), playerTntCountdown.get( player.getUniqueId() ) + 1 );
+				}
+			} else {
+				// its not, add it
+				playerTntCountdown.put( player.getUniqueId(), 0 );
+			}
 		}
 	}
 }
