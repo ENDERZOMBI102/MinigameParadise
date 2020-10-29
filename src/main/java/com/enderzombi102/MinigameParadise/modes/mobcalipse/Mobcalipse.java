575887811e297ba80249736f0e010571c7a35305
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-29 10:52:58 +0100

    virios fixes

--- src/main/java/com/enderzombi102/MinigameParadise/modes/mobcalipse/Mobcalipse.java
+++ src/main/java/com/enderzombi102/MinigameParadise/modes/mobcalipse/Mobcalipse.java
@@ -1,6 +1,7 @@
 package com.enderzombi102.MinigameParadise.modes.mobcalipse;
 
 import com.enderzombi102.MinigameParadise.MinigameParadise;
+import com.enderzombi102.MinigameParadise.Random;
 import com.enderzombi102.MinigameParadise.Util;
 import com.enderzombi102.MinigameParadise.modes.ModeBase;
 import lombok.SneakyThrows;
@@ -10,14 +11,14 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDropItemEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 
 public class Mobcalipse extends ModeBase {
@@ -33,7 +34,7 @@ public class Mobcalipse extends ModeBase {
 		Bukkit.getPluginManager().registerEvents(this.listener , MinigameParadise.instance);
 		broadcastPrefixedMessage("preparing the world..");
 		this.setupWorld();
-		broadcastPrefixedMessage("the Mobpcalipse is started! good luck!");
+		broadcastPrefixedMessage("the Mobcalipse is started! good luck!");
 	}
 
 	public void setupWorld() {
@@ -47,13 +48,6 @@ public class Mobcalipse extends ModeBase {
 			world.setMonsterSpawnLimit(300);
 			world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
 		}
-	}
-
-	public ItemStack randomizeDrop(Item item) {
-		ItemStack itemStack = item.getItemStack();
-		int amount = new Random().nextInt( 10 );
-		itemStack.setAmount( amount );
-		return itemStack;
 	}
 
 	@Override
@@ -70,68 +64,40 @@ public class Mobcalipse extends ModeBase {
 
 		@EventHandler
 		public void OnPlayerSleep(PlayerBedLeaveEvent evt) {
-			if ( Util.randomPercent(1) ) evt.getBed().breakNaturally();
+			if ( Random.possibility(1) ) evt.getBed().breakNaturally();
 		}
 
 		@EventHandler
 		public void OnEntityDrop(EntityDropItemEvent evt) {
-			Bukkit.broadcastMessage(evt.getItemDrop().getItemStack().toString());
-			ItemStack drop = randomizeDrop( evt.getItemDrop() );
-			evt.getItemDrop().setItemStack( drop );
+			Bukkit.broadcastMessage( evt.getItemDrop().getItemStack().toString() ); // debug
+			ItemStack itemStack = evt.getItemDrop().getItemStack();
+			// randomize item drop count
+			int amount = Random.randomInt(10);
+			itemStack.setAmount( amount );
+			evt.getItemDrop().setItemStack( itemStack );
 		}
 
+		@SuppressWarnings("unchecked")
 		@SneakyThrows
 		@EventHandler
-		public void onEntitySpawn(EntitySpawnEvent evt) {
+		public void onEntitySpawn(CreatureSpawnEvent evt) {
 			boolean editWeapon = true;
 			switch ( evt.getEntityType() ) {
 				// all those mobs will not be affected
 				case PLAYER:
-				case BOAT:
-				case ARROW:
-				case MINECART_MOB_SPAWNER:
-				case AREA_EFFECT_CLOUD:
-				case MINECART_FURNACE:
-				case THROWN_EXP_BOTTLE:
 				case WANDERING_TRADER:
-				case MINECART_COMMAND:
-				case MINECART_HOPPER:
-				case SPECTRAL_ARROW:
-				case MINECART_CHEST:
-				case EXPERIENCE_ORB:
-				case SHULKER_BULLET:
-				case SPLASH_POTION:
-				case FALLING_BLOCK:
 				case TRADER_LLAMA:
-				case ENDER_CRYSTAL:
-				case WITHER_SKULL:
 				case MUSHROOM_COW:
-				case MINECART_TNT:
-				case FISHING_HOOK:
-				case DROPPED_ITEM:
-				case LEASH_HITCH:
 				case EVOKER_FANGS:
-				case ENDER_SIGNAL:
-				case ENDER_PEARL:
 				case ARMOR_STAND:
-				case PRIMED_TNT:
-				case LLAMA_SPIT:
-				case ITEM_FRAME:
-				case LIGHTNING:
-				case SNOWBALL:
 				case UNKNOWN:
-				case FIREWORK:
-				case MINECART:
-				case PAINTING:
-				case TRIDENT:
 				case STRIDER:
 				case VILLAGER:
 				case DOLPHIN:
-				case EGG:
 				case BAT:
 				case CAT:
 					return;
-					// will have a target
+				// will have a target
 				case COD:
 				case COW:
 				case FOX:
@@ -158,61 +124,61 @@ public class Mobcalipse extends ModeBase {
 					return;
 				case CREEPER:
 					Creeper creeper = (Creeper) evt.getEntity();
-					creeper.setPowered( Util.randomPercent(4) );
-					creeper.setExplosionRadius( Util.randomInt(20, 1) );
-					creeper.setSilent( Util.randomPercent(20) );
-					creeper.setGlowing( Util.randomPercent(3) );
+					creeper.setPowered( Random.possibility(4) );
+					creeper.setExplosionRadius( Random.randomInt(20, 1) );
+					creeper.setSilent( Random.possibility(20) );
+					creeper.setGlowing( Random.possibility(3) );
 					return;
 				case HUSK:
 					Husk husk = (Husk) evt.getEntity();
-					husk.setBaby( Util.randomPercent(47) );
-					husk.setSilent( Util.randomPercent(20) );
-					husk.setGlowing( Util.randomPercent(3) );
+					husk.setBaby( Random.possibility(47) );
+					husk.setSilent( Random.possibility(20) );
+					husk.setGlowing( Random.possibility(3) );
 					break;
 				case ZOMBIE:
 					Zombie zombie = (Zombie) evt.getEntity();
-					zombie.setBaby( Util.randomPercent(46) );
-					zombie.setSilent( Util.randomPercent(20) );
-					zombie.setGlowing( Util.randomPercent(3) );
-					if ( Util.randomPercent(30) ) zombie.setHealth( Util.randomInt(100, 20) );
-					else zombie.setHealth( Util.randomInt(40, 20) );
+					zombie.setBaby( Random.possibility(46) );
+					zombie.setSilent( Random.possibility(20) );
+					zombie.setGlowing( Random.possibility(3) );
+					if ( Random.possibility(30) ) zombie.setHealth( Random.randomInt(100, 20) );
+					else zombie.setHealth( Random.randomInt(40, 20) );
 					break;
 				case WITCH:
 					Witch witch = (Witch) evt.getEntity();
-					witch.setHealth( Util.randomInt(40, 20) );
-					witch.setSilent( Util.randomPercent(20) );
-					witch.setGlowing( Util.randomPercent(3) );
+					witch.setHealth( Random.randomInt(40, 20) );
+					witch.setSilent( Random.possibility(20) );
+					witch.setGlowing( Random.possibility(3) );
 					break;
 				case PHANTOM:
 					Phantom phantom = (Phantom) evt.getEntity();
-					if ( Util.randomPercent(10) ) phantom.setSize( Util.randomInt(40, 2) );
+					if ( Random.possibility(10) ) phantom.setSize( Random.randomInt(40, 2) );
 					phantom.setCanPickupItems(true);
-					phantom.setSilent( Util.randomPercent(30) );
-					phantom.setGlowing( Util.randomPercent(3) );
+					phantom.setSilent( Random.possibility(30) );
+					phantom.setGlowing( Random.possibility(3) );
 					break;
 				case ENDERMITE:
 					Endermite endermite = (Endermite) evt.getEntity();
-					endermite.setInvulnerable( Util.randomPercent(1) );
+					endermite.setInvulnerable( Random.possibility(1) );
 					break;
 				case SKELETON:
 					Skeleton skeleton = (Skeleton) evt.getEntity();
-					skeleton.setInvisible( Util.randomPercent(10) );
-					skeleton.setSilent( Util.randomPercent(20) );
-					skeleton.setGlowing( Util.randomPercent(3) );
-					skeleton.setHealth( Util.randomInt(50, 20) );
+					skeleton.setInvisible( Random.possibility(10) );
+					skeleton.setSilent( Random.possibility(20) );
+					skeleton.setGlowing( Random.possibility(3) );
+					skeleton.setHealth( Random.randomInt(50, 20) );
 					break;
 				case PIGLIN:
 					Piglin piglin = (Piglin) evt.getEntity();
 					ItemStack piglinItem = piglin.getEquipment().getItemInMainHand();
-					if ( Util.randomPercent(40) ) {
+					if ( Random.possibility(40) ) {
 						List<ItemStack> weapons = ( (List<ItemStack>) Util.weapon.clone() )
 								.stream().filter(itemStack -> itemStack.getType() == Material.BOW)
 								.collect( Collectors.toList() );
-						piglinItem.setType( Util.randomEntry( weapons ).getType() );
+						piglinItem.setType( Random.randomEntry( weapons ).getType() );
 					}
-					if ( Util.randomPercent(50) ) {
-						Enchantment enchant = Util.randomEntry( Util.weapon_enchants );
-						if (enchant != null) piglinItem.addEnchantment( enchant, Util.randomInt(20) );
+					if ( Random.possibility(50) ) {
+						Enchantment enchant = Random.randomEntry( Util.weapon_enchants );
+						if (enchant != null) piglinItem.addEnchantment( enchant, Random.randomInt(20) );
 					}
 					piglin.getEquipment().setItemInMainHand( piglinItem );
 					editWeapon = false;
@@ -222,11 +188,11 @@ public class Mobcalipse extends ModeBase {
 			LivingEntity entity = (LivingEntity) evt.getEntity();
 			entity.getEquipment().setArmorContents( randomArmor() );
 			if (editWeapon) {
-				ItemStack weapon = Util.randomEntry( Util.weapon );
+				ItemStack weapon = Random.randomEntry( Util.weapon );
 				if (weapon != null) {
 					weapon.addEnchantment(
-							Util.randomEntry( Util.weapon_enchants), // enchant
-							Util.randomInt(5 ) // level
+							Random.randomEntry( Util.weapon_enchants), // enchant
+							Random.randomInt(5 ) // level
 					);
 				}
 				entity.getEquipment().setItemInMainHand(weapon);
@@ -272,13 +238,14 @@ public class Mobcalipse extends ModeBase {
 		}
 	}
 
-	// returns random choosen armor (diamond, chain, leather, gold, iron, null)
+	// returns random chosen armor (diamond, chain, leather, gold, iron, null)
+	@NotNull
 	private ItemStack[] randomArmor() {
 		ItemStack[] armor = {null, null, null, null};
-		armor[3] = Util.randomEntry( Util.helmet );
-		armor[2] = Util.randomEntry( Util.chestplate );
-		armor[1] = Util.randomEntry( Util.leggins );
-		armor[0] = Util.randomEntry( Util.boots );
+		armor[3] = Random.randomEntry( Util.helmet );
+		armor[2] = Random.randomEntry( Util.chestplate );
+		armor[1] = Random.randomEntry( Util.leggins );
+		armor[0] = Random.randomEntry( Util.boots );
 		return armor;
 	}
 }
