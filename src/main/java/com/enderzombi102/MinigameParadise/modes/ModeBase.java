eaacaa9363b2baf5122cae6ae0826442ff0f708d
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-02-20 20:52:58 +0100

    added a per-mode logger
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/main/java/com/enderzombi102/MinigameParadise/modes/ModeBase.java
+++ src/main/java/com/enderzombi102/MinigameParadise/modes/ModeBase.java
@@ -1,9 +1,13 @@
 package com.enderzombi102.MinigameParadise.modes;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bukkit.Bukkit;
 
 public abstract class ModeBase {
-	
+
+	protected Logger logger = LogManager.getLogger( getClass().getSimpleName() );
+
 	protected void broadcastPrefixedMessage(String text) {
 		Bukkit.getServer().broadcastMessage("["+this.getClass().getSimpleName()+"] "+text);
 	}
